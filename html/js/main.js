54ad64730a18a548c0efe7942a91795b44ae33f9
root <root@DESKTOP-9T6P5NF.localdomain>
2019-05-15 22:31:18 +0800

    add days filter to author stats

--- html/js/main.js
+++ html/js/main.js
@@ -1,11 +1,70 @@
+// 时间处理帮助函数
+const timeHelper = {
+  all: function () {
+    return 0
+  },
+  // 昨天
+  prevDay: function () {
+    let yesterday = moment().add(-1, 'days').format('YYYY-MM-DD 00:00:00')
+    return parseInt( new Date(yesterday).getTime() / 1000)
+  },
+
+  // 本周周一
+  thisWeek: function () {
+    let today = (new Date()).getDay()
+    let monday = moment().add(1- today, 'days').format('YYYY-MM-DD 00:00:00')
+    return parseInt( new Date(monday).getTime() / 1000)
+  },
+
+  // 上周周一
+  prevWeek: function () {
+    let today = (new Date()).getDay()
+    let monday = moment().add(1 - 7 - today, 'days').format('YYYY-MM-DD 00:00:00')
+    return parseInt( new Date(monday).getTime() / 1000)
+  },
+  // 本月1号
+  thisMonth: function () {
+    let month = moment().format('YYYY-MM-01 00:00:00')
+    return parseInt(new Date(month).getTime() / 1000)
+  },
+  // 上月1号
+  prevMonth: function () {
+    let month = moment().add(-1, 'months').format('YYYY-MM-01 00:00:00')
+    return parseInt(new Date(month).getTime() / 1000)
+  }
+}
+
+
 var app = new Vue({
   el: '#app',
   data: {
-    gitdata: window.JSONDATA
+    gitdata: window.JSONDATA,
+    view: 'author',
+    views: [
+      {
+        key: 'author',
+        name: '开发者'
+      }, {
+        key: 'codes',
+        name: '代码量'
+      }
+    ],
+    daylist: {
+      prevDay: '昨天',
+      thisWeek: '本周',
+      prevWeek: '上周',
+      thisMonth: '本月',
+      prevMonth: '上月',
+      all: '全部'
+    },
+    startday: 'all'// 从哪天开始（时间戳）
+
   },
   computed: {
     authors: function () {
-      let data = this.gitdata.reduce((result, item) => {
+      let data = this.gitdata.filter(item => {
+       return item.time >= timeHelper[this.startday]()
+      }).reduce((result, item) => {
         result[item.author] =  result[item.author] || {
           commits: 0,
           '+lines': 0,
@@ -30,6 +89,8 @@ var app = new Vue({
       })
       return arr
     }
+  },
+  methods: {
   },
   created () {
   }
